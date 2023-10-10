c733d68ec74844d347dff6100ff908ebffeec71b
Earl <earlmunyua96@gmail.com>
2023-10-10 11:53:47 +0300

    array.filter

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers, name) {
+ if (drivers === name) {
+    return `${name}`
+ }
+return drivers.filter((array) => 
+array.toLowerCase() === name.toLowerCase())
+}
+
+function fuzzyMatch(drivers, letters) {
+return drivers.filter((array) => array.startsWith(letters))
+}
+
+function matchName(drivers, name) {
+    return drivers.filter((array) => 
+        array.name === name
+    )
+}
+
